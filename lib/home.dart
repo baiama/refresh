f835d9edc44df41586fdd54fb6adbfb89791cfba
baiama <primkulovbaiaman@gmail.com>
2021-10-20 14:28:24 +0600

    dsad

--- lib/home.dart
+++ lib/home.dart
@@ -10,41 +10,21 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Refresh'),
       ),
-      body: PageView(
-        children: [
-          RefreshIndicator(
-            onRefresh: () async {
-              await Future.delayed(Duration(seconds: 3));
-            },
-            child: ListView.builder(
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-                    child: Container(
-                      height: 40,
-                      color: Colors.green,
-                    ),
-                  );
-                }),
-          ),
-          VibrationRefreshIndicator(
-            onRefresh: () async {
-              await Future.delayed(Duration(seconds: 3));
-            },
-            child: ListView.builder(
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-                    child: Container(
-                      height: 40,
-                      color: Colors.yellow,
-                    ),
-                  );
-                }),
-          )
-        ],
+      body: VibrationRefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(Duration(seconds: 3));
+        },
+        child: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: Container(
+                  height: 40,
+                  color: Colors.yellow,
+                ),
+              );
+            }),
       ),
     );
   }
