c36eff4ee939cd1eac6f2583433d1293a5b74f2a
baiama <31430512+baiama@users.noreply.github.com>
2021-10-20 14:31:10 +0600

    Task 1 (#1)

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:refresh_layout/refresh.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +10,26 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Refresh'),
+      ),
+      body: VibrationRefreshIndicator(
+        //distance between child and top default 100
+        childOffset: 100,
+        //vibration types default FeedbackType.medium
+        feedbackType: FeedbackType.medium,
+        onRefresh: () async {
+          await Future.delayed(Duration(seconds: 3));
+        },
+        child: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: Container(
+                  height: 40,
+                  color: Colors.yellow,
+                ),
+              );
+            }),
       ),
     );
   }
